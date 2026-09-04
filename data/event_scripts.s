ed289f4bafd2ce199a73e34069e90030e84bd1f2
fisham33 <fisham33@gmail.com>
2026-09-04 23:56:02 +1000

    initial maps

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -1766,3 +1766,7 @@ EventScript_PalletTown_PlayersHouse_2F_TurnOnPC::
 	.include "data/maps/tarc3_GozoRoom/scripts.inc"
 
 	.include "data/maps/tarc3_ElectricalRoom/scripts.inc"
+
+	.include "data/maps/tarc3_Destination/scripts.inc"
+
+	.include "data/maps/tarc3_DestinationDock/scripts.inc"
